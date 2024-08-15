776907be752117daa90d561212b01fcc5ee983b7
devmianharoon <devmianharoon@gmail.com>
2024-08-15 12:04:10 +0500

    user dto

--- src/users/data/user.dto.ts
+++ src/users/data/user.dto.ts
@@ -1,10 +1,11 @@
+// for create user dto
 export class UserDto{
     id : number
     name :string 
     email : string
     password : string
 }
-
+// for login Dto
 export class LoginDto{
     email: string;
     password: string

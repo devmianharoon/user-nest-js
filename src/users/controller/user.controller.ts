1431575ceb6183c30e3be4c8b6aae53e94d7190f
devmianharoon <devmianharoon@gmail.com>
2024-08-17 14:27:36 +0500

    controller

--- src/users/controller/user.controller.ts
+++ src/users/controller/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserService } from '../services/user.service';
 import { LoginDto, UserDto } from '../data/user.dto';
@@ -18,17 +18,19 @@ export class UserController {
     return this.userService.getUser()
   }
   @Get(':id')
-  async getUserId(@Param('id')id:number){
+  async getUserId(@Param('id',ParseIntPipe)id:number){
+    console.log(id , typeof(id));  // pipe  used for restrict typing 
+    
     return this.userService.getUserId(id)
   }
   @Patch(':id')
-  async updateUser(@Param('id')id:number,@Body()UserDto:UserDto){
+  async updateUser(@Param('id',ParseIntPipe)id:number,@Body()UserDto:UserDto){
     await this.userService.updateUser(id, UserDto)
     return "User Updated Sucessfully" 
 
   }
-  @Delete(':id')
-  async delUser(@Param('id')id:number){
+  @Delete(':id')  
+  async delUser(@Param('id',ParseIntPipe)id:number){
     await this.userService.delUser(id)
     return "User deleted Sucessfully"
   }
